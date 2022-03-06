aab443222c87463dc460f2c6478d2e6a59b22d25
AJDewy <adam32thomas@gmail.com>
2022-03-06 17:51:58 -0500

    3.1.5 complete

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+
+
+let playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
