c53acf5b44bb583aabffc5cb85ce5c01f76c2265
AJDewy <adam32thomas@gmail.com>
2022-03-27 20:52:15 -0400

    3.1.7 progress

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,34 @@
 
 
 let playerName = window.prompt("What is your robot's name?");
-// What is this?
+let playerHealth = 100;
+let playerAttack = 10;
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
 console.log("Our robot's name is " + playerName);
+window.alert(playerName);
 
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!");
+let fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + " now has " + enemyHealth + " health remaining. ");
+    playerHealth = playerHealth - enemyAttack;
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    console.log(enemyName + " attacked " + playerName + " now has " + playerHealth + " health remaining. ");
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-// fight();
+
+fight();
